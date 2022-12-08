8b7410b9b8da538f07467fca9b3aef7625ce9855
David Fernandez <david@108.218.21.198.tigernet.wifi.dyn.clemson.edu>
2022-12-07 20:42:35 -0500

    Fixes q3.js indentation

--- q3.js
+++ q3.js
@@ -1,73 +1,73 @@
-// set the dimensions and margins of the graph
-var q3_box = document.getElementsByClassName("q3")
-const margin = {top: 50, right: 100, bottom: 20, left: 100},
-    width = q3_box[0].clientWidth - margin.left - margin.right,
-    height = q3_box[0].clientHeight - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-const svg = d3.select("#my_dataviz")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
-
-var slider = document.getElementById("slider");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
 
 // Parse the Data
 d3.csv("df7.csv").then( function(data) {
+  // set the dimensions and margins of the graph
+  var q3_box = document.getElementsByClassName("q3")
+  const margin = {top: 50, right: 100, bottom: 20, left: 100},
+    width = q3_box[0].clientWidth - margin.left - margin.right,
+    height = q3_box[0].clientHeight - margin.top - margin.bottom;
+
+  // append the svg object to the body of the page
+  const svg = d3.select("#my_dataviz")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  var slider = document.getElementById("slider");
+  var output = document.getElementById("demo");
+  output.innerHTML = slider.value;
+
+  slider.oninput = function() {
+    output.innerHTML = this.value;
+  }
 
   // List of subgroups = header of the csv files = soil condition here
   const subgroups = data.columns.slice(1)
   console.log(subgroups)
 
-//   var slider = document.getElementById("slider");
-//   var output = document.getElementById("demo");
-//   output.innerHTML = slider.value;
+  //   var slider = document.getElementById("slider");
+  //   var output = document.getElementById("demo");
+  //   output.innerHTML = slider.value;
 
-//   slider.oninput = function() {
-//   output.innerHTML = this.value;
+  //   slider.oninput = function() {
+  //   output.innerHTML = this.value;
 
-// d3.select("#slider")
-// .on("input", function() {
-//     var bisect = d3.bisector(function (d){
-//         return d.yearID
-//       }).left
-    
-//       var idx = bisect(data, +this.value)
-//       console.log("idx: ",idx)
-// }
+  // d3.select("#slider")
+  // .on("input", function() {
+  //     var bisect = d3.bisector(function (d){
+  //         return d.yearID
+  //       }).left
 
-// )
-//   var bisect = d3.bisector(function (d){
-//     return d.yearID
-//   }).left
+  //       var idx = bisect(data, +this.value)
+  //       console.log("idx: ",idx)
+  // }
 
-//   var idx = bisect(data, yearID.toString())
-//   console.log("idx: ",idx)
-  
+  // )
+  //   var bisect = d3.bisector(function (d){
+  //     return d.yearID
+  //   }).left
+
+  //   var idx = bisect(data, yearID.toString())
+  //   console.log("idx: ",idx)
+
 
   // List of groups = species here = value of the first column called group -> I show them on the X axis
   const groups = data.map(d => (d.yearID))
 
   // Add X axis
   var x = d3.scaleBand()
-      .domain(groups)
-      .range([0, width])
-      .padding([0.2])
+    .domain(groups)
+    .range([0, width])
+    .padding([0.2])
   svg.append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(d3.axisBottom(x).tickSizeOuter(0));
 
   // Add Y axis
   var y = d3.scaleLinear()
-    // .domain([0, 60])
+  // .domain([0, 60])
     .domain([0, 10000000])
     .range([ height, 0 ]);
   svg.append("g")
@@ -81,112 +81,117 @@ d3.csv("df7.csv").then( function(data) {
   //stack the data? --> stack per subgroup
   const stackedData = d3.stack()
     .keys(subgroups)
-    (data)
+  (data)
 
-    console.log("stacked:",stackedData)
+  console.log("stacked:",stackedData)
 
-    // ----------------
+  // ----------------
   // Create a tooltip
   // ----------------
   const tooltip = d3.select("#my_dataviz")
-  .append("div")
-  .style("opacity", 0)
-  .attr("class", "tooltip")
-  .style("background-color", "white")
-  .style("border", "solid")
-  .style("border-width", "1px")
-  .style("border-radius", "5px")
-  .style("padding", "10px")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "10px")
 
-// // Three function that change the tooltip when user hover / move / leave a cell
-// const mouseover = function(event, d) {
-//   const subgroupName = d3.select(this.parentNode).datum().key;
-//   const subgroupValue = d.data[subgroupName];
-//   // Reduce opacity of all rect to 0.2
-//   d3.selectAll("rect").style("opacity", 0.2)
+  // // Three function that change the tooltip when user hover / move / leave a cell
+  // const mouseover = function(event, d) {
+  //   const subgroupName = d3.select(this.parentNode).datum().key;
+  //   const subgroupValue = d.data[subgroupName];
+  //   // Reduce opacity of all rect to 0.2
+  //   d3.selectAll("rect").style("opacity", 0.2)
 
-//   // Highlight all rects of this subgroup with opacity 1. It is possible to select them since they have a specific class = their name.
-//    d3.selectAll("."+subGroupName).style("opacity",1)
-// //   tooltip
-// //       .html("League: " + subgroupName + "<br>" + "Salary: " + d3.format(",.1f")(subgroupValue))
-// //       .style("opacity", 1)
-    
-    
+  //   // Highlight all rects of this subgroup with opacity 1. It is possible to select them since they have a specific class = their name.
+  //    d3.selectAll("."+subGroupName).style("opacity",1)
+  // //   tooltip
+  // //       .html("League: " + subgroupName + "<br>" + "Salary: " + d3.format(",.1f")(subgroupValue))
+  // //       .style("opacity", 1)
 
-// // }
-// const mousemove = function(event, d) {
-//   tooltip.style("transform","translateY(-55%)")
-//          .style("left",(event.x)/2+"px")
-//          .style("top",(event.y)/2-30+"px")
-// }
-// const mouseleave = function(event, d) {
-//   tooltip
-//     .style("opacity", 0)
-// }
 
+
+  // // }
+  // const mousemove = function(event, d) {
+  //   tooltip.style("transform","translateY(-55%)")
+  //          .style("left",(event.x)/2+"px")
+  //          .style("top",(event.y)/2-30+"px")
+  // }
+  // const mouseleave = function(event, d) {
+  //   tooltip
+  //     .style("opacity", 0)
+  // }
+
+  d3.select("#year")
+    .on("change", function() {
+      console.log("here")
+      console.log("value", this.value);
+    });
 
 
 
   // Show the bars
   svg.append("g")
     .selectAll("g")
-    // Enter in the stack data = loop key per key = group per group
+  // Enter in the stack data = loop key per key = group per group
     .data(stackedData)
     .join("g")
-      .attr("fill", d => color(d.key))
-      .attr("class", d => "myRect " + d.key ) // Add a class to each subgroup: their name
-      .selectAll("rect")
-      // enter a second time = loop subgroup per subgroup to add all rectangles
-      .data(d => d)
-      .join("rect")
-        .attr("x", d => x(d.data.yearID))
-        .attr("y", d => y(d[1]))
-        .attr("height", d => y(d[0]) - y(d[1]))
-        .attr("width",x.bandwidth())
-        .on("mouseover", function (event,d) { // What happens when user hover a bar
+    .attr("fill", d => color(d.key))
+    .attr("class", d => "myRect " + d.key ) // Add a class to each subgroup: their name
+    .selectAll("rect")
+  // enter a second time = loop subgroup per subgroup to add all rectangles
+    .data(d => d)
+    .join("rect")
+    .attr("x", d => x(d.data.yearID))
+    .attr("y", d => y(d[1]))
+    .attr("height", d => y(d[0]) - y(d[1]))
+    .attr("width",x.bandwidth())
+    .on("mouseover", function (event,d) { // What happens when user hover a bar
 
-          // what subgroup are we hovering?
-          const subGroupName = d3.select(this.parentNode).datum().key
+      // what subgroup are we hovering?
+      const subGroupName = d3.select(this.parentNode).datum().key
 
-          // Reduce opacity of all rect to 0.2
-           d3.selectAll(".myRect").style("opacity", 0.2)
+      // Reduce opacity of all rect to 0.2
+      d3.selectAll(".myRect").style("opacity", 0.2)
 
-          // Highlight all rects of this subgroup with opacity 1. It is possible to select them since they have a specific class = their name.
-           d3.selectAll("."+subGroupName).style("opacity",1)
-        })
-        .on("mouseleave", function (event,d) { // When user do not hover anymore
+      // Highlight all rects of this subgroup with opacity 1. It is possible to select them since they have a specific class = their name.
+      d3.selectAll("."+subGroupName).style("opacity",1)
+    })
+    .on("mouseleave", function (event,d) { // When user do not hover anymore
 
-          // Back to normal opacity: 1
-          d3.selectAll(".myRect")
-          .style("opacity",1)
-      })
-    // .on("mouseover", mouseover)
-    // .on("mousemove", mousemove)
-    // .on("mouseleave", mouseleave)
+      // Back to normal opacity: 1
+      d3.selectAll(".myRect")
+        .style("opacity",1)
+    })
+  // .on("mouseover", mouseover)
+  // .on("mousemove", mousemove)
+  // .on("mouseleave", mouseleave)
 
-// A function that update the chart
-function update(selectedGroup) {
+  // A function that update the chart
+  function update(selectedGroup) {
 
     // d3.selectAll("rect").style("opacity", 0.2)
 
     // d3.selectAll("."+groups)
     //   .style("opacity", 1)
-    var subgroupName = d3.select(this.parentNode).datum().key; // This was the tricky part
-    var subgroupValue = d.data[subgroupName];
-    // Reduce opacity of all rect to 0.2
-    d3.selectAll(".myRect").style("opacity", 0.2)
+    //var subgroupName = d3.select(this.parentNode).datum().key; // This was the tricky part
+    //var subgroupValue = d.data[subgroupName];
+    //// Reduce opacity of all rect to 0.2
+    //d3.selectAll(".myRect").style("opacity", 0.2)
     // Highlight all rects of this subgroup with opacity 0.8. It is possible to select them since they have a specific class = their name.
-    d3.selectAll("."+subgroupName)
-      .style("opacity", 1)
-    
-}
+    //d3.selectAll("."+subgroupName)
+    //  .style("opacity", 1)
 
-//On change, update styling
-d3.select("input")
-.on("change", function() {
-  update(this.value);
-  // newbar1.exit().remove();
-});
+  }
+
+  //On change, update styling
+  d3.select("input")
+    .on("change", function() {
+      update(this.value);
+      // newbar1.exit().remove();
+    });
 
 
 
