8972e50e51a3f23eff464add9aab79bf33c1960c
Rayid Ali <42346777+rayidali@users.noreply.github.com>
2023-08-01 20:53:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// You can add interactivity to your website with JavaScript.
+// For example, you can show a welcome message when the website is loaded.
+
+window.onload = function() {
+    alert('Welcome to Rayid Ali\'s personal website!');
+};
