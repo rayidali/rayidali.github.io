2a884e1984227dac3ada0ed21d6523cd67b894c4
Rayid Ali <42346777+rayidali@users.noreply.github.com>
2025-02-24 21:50:57 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// You can add interactivity to your website with JavaScript.
-// For example, you can show a welcome message when the website is loaded.
-
-window.onload = function() {
-    alert('Welcome to Rayid Ali\'s personal website!');
-};
